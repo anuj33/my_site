cbf169d6124a5c6a639b8f7b2b3efa43f0fbb1a9
anuj <anujlnmiit@gmail.com>
2017-07-10 22:45:09 +0530

    final_site_using_html/css/js

---
+++ thi.js
@@ -0,0 +1,92 @@
+var a=document.getElementById("banner");
+var b=document.getElementById("headerchange");
+var c=document.getElementById("parachange");
+
+function changetext1() {
+    a.style.background= "#4b8be2";
+    b.innerHTML="good anuj good";
+    c.innerHTML="WebStorm supports an operation tha" +
+        "t is somehow opposite to live editing. During a " +
+        "debugging session, the Elements tab of the Debug tool" +
+        " window shows the HTML source code that implements the actual browser page and its HTML DOM structure. Moreover, any changes made to the page through the browser are immediately " +
+        "reflected in the Elements tab. For more details, " +
+        "see Viewing Actual HTML DOM.";
+
+
+
+}
+
+function changetext2() {
+    a.style.background= "#e266b4";
+    b.innerHTML="good anuj good";
+    c.innerHTML="WebStorm supports an operation tha" +
+        "t is somehow opposite to live editing. During a " +
+        "debugging session, the Elements tab of the Debug tool" +
+        " window shows the HTML source code that implements the actual browser page and its HTML DOM structure. Moreover, any changes made to the page through the browser are immediately " +
+        "reflected in the Elements tab. For more details, " +
+        "see Viewing Actual HTML DOM.";
+}
+
+function changetext3() {
+    a.style.background = "#41e2d6";
+    b.innerHTML = "good anuj good";
+    c.innerHTML = "WebStorm supports an operation tha" +
+        "t is somehow opposite to live editing. During a " +
+        "debugging session, the Elements tab of the Debug tool" +
+        " window shows the HTML source code that implements the actual browser page and its HTML DOM structure. Moreover, any changes made to the page through the browser are immediately " +
+        "reflected in the Elements tab. For more details, " +
+        "see Viewing Actual HTML DOM.";
+
+}
+
+    function changetext4() {
+        a.style.background= "#e24328";
+        b.innerHTML="good anuj good";
+        c.innerHTML="WebStorm supports an operation tha" +
+            "t is somehow opposite to live editing. During a " +
+            "debugging session, the Elements tab of the Debug tool" +
+            " window shows the HTML source code that implements the actual browser page and its HTML DOM structure. Moreover, any changes made to the page through the browser are immediately " +
+            "reflected in the Elements tab. For more details, " +
+            "see Viewing Actual HTML DOM.";
+
+
+
+    }
+    function changetext5() {
+        a.style.background= "#3e19e2";
+        b.innerHTML="good anuj good";
+        c.innerHTML="WebStorm supports an operation tha" +
+            "t is somehow opposite to live editing. During a " +
+            "debugging session, the Elements tab of the Debug tool" +
+            " window shows the HTML source code that implements the actual browser page and its HTML DOM structure. Moreover, any changes made to the page through the browser are immediately " +
+            "reflected in the Elements tab. For more details, " +
+            "see Viewing Actual HTML DOM.";
+
+
+
+    }
+    function changetext6() {
+        a.style.background = "#13e2a2";
+        b.innerHTML = "good anuj good";
+        c.innerHTML = "WebStorm supports an operation tha" +
+            "t is somehow opposite to live editing. During a " +
+            "debugging session, the Elements tab of the Debug tool" +
+            " window shows the HTML source code that implements the actual browser page and its HTML DOM structure. Moreover, any changes made to the page through the browser are immediately " +
+            "reflected in the Elements tab. For more details, " +
+            "see Viewing Actual HTML DOM.";
+
+
+    }
+        function changetext7(){
+            a.style.background = "#b7e20a";
+            b.innerHTML = "good anuj good";
+            c.innerHTML = "WebStorm supports an operation tha" +
+                "t is somehow opposite to live editing. During a " +
+                "debugging session, the Elements tab of the Debug tool" +
+                " window shows the HTML source code that implements the actual browser page and its HTML DOM structure. Moreover, any changes made to the page through the browser are immediately " +
+                "reflected in the Elements tab. For more details, " +
+                "see Viewing Actual HTML DOM.";
+
+
+    }
+
